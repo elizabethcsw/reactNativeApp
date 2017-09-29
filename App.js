784fa52a8d33989ebf16b296b5b8e39caf3525f8
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-09-29 02:06:38 +0100

    Maps and allow location permission

--- App.js
+++ App.js
@@ -1,15 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Constants, MapView, Location, Permissions} from 'expo';
+
+
+class CounterButton extends React.Component {
+  state = {
+    counter: 0,
+  }
+  render() {
+
+    return (
+
+        <TouchableOpacity
+        style={{ backgroundColor: 'white'}}
+        onPress = {() => this.setState({ counter: this.state.counter +1})}>
+        <Text style={styles.paragraph}>
+          {this.state.counter}
+        </Text>
+        </TouchableOpacity>
+    );
+  }
+}
 
 export default class App extends React.Component {
+  state = {
+    counter: 0,
+    mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
+    locationResult: null
+  };
+
+  componentDidMount() {
+   this._getLocationAsync();
+ };
+
+  _handleMapRegionChange = mapRegion => {
+  this.setState({ mapRegion });
+
+};
+
+_getLocationAsync = async () => {
+ let { status } = await Permissions.askAsync(Permissions.LOCATION);
+ if (status !== 'granted') {
+   this.setState({
+     locationResult: 'Permission to access location was denied',
+   });
+ }
+
+ let location = await Location.getCurrentPositionAsync({});
+ this.setState({ locationResult: JSON.stringify(location) },
+);
+  console.log(mapRegion)
+ console.log(location.coords.altitude);
+};
+
+
+
   render() {
-    console.log('logs are forwarded too!!!!!!!!!!!')
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>HI THERE OMG HELLO THIS IS MY FIRST TRY</Text>
+
+      <MapView
+         style={{ alignSelf: 'stretch', height: 300 }}
+         region={this.state.mapRegion}
+         onRegionChange={this._handleMapRegionChange}
+       />
+        <View style={{ flexDirection: 'row'}}>
+          <CounterButton/>
+          <CounterButton/>
+          <CounterButton/>
+        </View>
       </View>
     );
   }
@@ -18,8 +77,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gold',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
   },
 });
