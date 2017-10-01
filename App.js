988bb0014d8f79fe94fb2050424e42b22434c5b2
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-10-01 16:41:54 +0100

    open local photos directory, add texts on top of photo

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Constants, MapView, Location, Permissions} from 'expo';
+import React from "react";
+import Expo from "expo";
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image, TextInput } from "react-native";
+import { Constants, MapView, Location, Permissions } from "expo";
+import ExpoTHREE from "expo-three";
 
+const THREE = require("three");
+
+// `THREEView` wraps a `GLView` and creates a THREE renderer that uses
+// that `GLView`. The class needs to be constructed with a factory so that
+// the `THREE` module can be injected without expo-sdk depending on the
+// `'three'` npm package.
 
 class CounterButton extends React.Component {
   state = {
-    counter: 0,
-  }
+    counter: 0
+  };
   render() {
-
     return (
-
-        <TouchableOpacity
-        style={{ backgroundColor: 'white'}}
-        onPress = {() => this.setState({ counter: this.state.counter +1})}>
-        <Text style={styles.paragraph}>
-          {this.state.counter}
-        </Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        style={{ backgroundColor: "white" }}
+        onPress={() => this.setState({ counter: this.state.counter + 1 })}
+      >
+        <Text style={styles.paragraph}>{this.state.counter}</Text>
+      </TouchableOpacity>
     );
   }
 }
@@ -25,68 +30,148 @@ class CounterButton extends React.Component {
 export default class App extends React.Component {
   state = {
     counter: 0,
-    mapRegion: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421 },
-    locationResult: null
+    mapRegion: {
+      latitude: 51.52215262,
+      longitude: -0.224044,
+      latitudeDelta: 0.4695249,
+      longitudeDelta: 1.0413547
+    },
+    locationResult: null,
+    imageUri: 'https://images.plurk.com/2A3ca9h910J2q0IRdd8bQE.jpg',
+    topText: '',
+    bottomText: '',
   };
 
   componentDidMount() {
-   this._getLocationAsync();
- };
+    this._getLocationAsync();
+  }
 
   _handleMapRegionChange = mapRegion => {
-  this.setState({ mapRegion });
+    this.setState({ mapRegion });
+    console.log("mapregion");
+    console.log(mapRegion);
+  };
 
-};
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        locationResult: "Permission to access location was denied"
+      });
+    }
 
-_getLocationAsync = async () => {
- let { status } = await Permissions.askAsync(Permissions.LOCATION);
- if (status !== 'granted') {
-   this.setState({
-     locationResult: 'Permission to access location was denied',
-   });
- }
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ locationResult: JSON.stringify(location) });
 
- let location = await Location.getCurrentPositionAsync({});
- this.setState({ locationResult: JSON.stringify(location) },
-);
-  console.log(mapRegion)
- console.log(location.coords.altitude);
-};
-
-
+    console.log(location.coords.altitude);
+  };
 
   render() {
     return (
+      <ScrollView>
       <View style={styles.container}>
-
-      <MapView
-         style={{ alignSelf: 'stretch', height: 300 }}
-         region={this.state.mapRegion}
-         onRegionChange={this._handleMapRegionChange}
-       />
-        <View style={{ flexDirection: 'row'}}>
-          <CounterButton/>
-          <CounterButton/>
-          <CounterButton/>
+        <TextInput
+          style={styles.inputText}
+          onChangeText = {(text) => this.setState({topText: text})}
+          value={this.state.text}
+          />
+        <View style={{ margin: 5}}>
+          <Image
+            source={{
+              uri: this.state.imageUri
+            }}
+            style={{ width: 300, height: 300 }}
+          />
+          <Text
+            style={[styles.text, {top:5}]}>
+            {this.state.topText}
+          </Text>
+          <Text
+            style={[styles.text, {bottom:5}]}>
+            {this.state.bottomText}
+          </Text>
         </View>
+        <TextInput
+          style={styles.inputText}
+          onChangeText = {(text) => this.setState({bottomText: text})}
+          value={this.state.bottomText}
+          />
+
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this._onPick}>
+          <Text>hello, world</Text>
+        </TouchableOpacity>
+
+        <MapView
+          style={{ alignSelf: "stretch", height: 300 }}
+          region={this.state.mapRegion}
+          onRegionChange={this._handleMapRegionChange}
+        />
+
+        <View style={{ flexDirection: "row" }}>
+          <CounterButton />
+          <CounterButton />
+          <CounterButton />
+        </View>
+
+
+
+
       </View>
+      </ScrollView>
     );
+  }
+
+  _onPick = async () => {
+    const {
+      cancelled,
+      uri,
+    } = await Expo.ImagePicker.launchImageLibraryAsync();
+    if (!cancelled){
+      this.setState({imageUri: uri})
+    }
+    console.log('uri', uri)
   }
 }
 
 const styles = StyleSheet.create({
+  inputText: {
+    height: 40,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    margin: 10,
+    padding: 5
+  },
+  text: {
+    position: 'absolute',
+    left:50,
+    right:50,
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 20,
+    fontWeight: '900',
+    textAlign: 'center'
+  },
+  button: {
+    padding: 5,
+    backgroundColor: "#ddd",
+    margin: 5
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1"
   },
   paragraph: {
     margin: 24,
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-  },
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#34495e"
+  }
 });
